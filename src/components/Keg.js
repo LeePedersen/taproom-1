bd373fa789564bdb6296e2ab626a451452487545
Lee Pedersen <leepedersenr@gmail.com>
2020-02-14 09:09:58 -0800

    plan and outline, create kegs and keg components

---
+++ src/components/Keg.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Keg(props) {
+  return(
+    <div>
+    <p>
+
+  )
+}
+
+Month.propTypes = {
+  name: PropTypes.string.isRequired,
+  brand: PropTypes.string.isRequired,
+  price: PropTypes.int.isRequired,
+  alcoholContent: PropTypes.string.isRequired
+};
+
+export default Keg;
