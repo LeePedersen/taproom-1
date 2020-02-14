6a468ddefca383c902c881a9fe36ed0f88eb7135
Lee Pedersen <leepedersenr@gmail.com>
2020-02-14 10:07:03 -0800

    create app.js

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -4,8 +4,11 @@ import PropTypes from 'prop-types';
 function Keg(props) {
   return(
     <div>
-    <p>
-
+      <p>{props.name}</p>
+      <p>{props.brand}</p>
+      <p>{props.price}</p>
+      <p>{props.alcoholContent}</p>
+    </div>
   )
 }
 
